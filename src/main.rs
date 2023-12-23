09212b15549ba46e5e225e2588f99dc8fce83d7e
Eric Correia <correia.eh@gmail.com>
2023-12-22 22:47:12 -0500

    parse request and read path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,11 +12,69 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let _ = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+                let mut buffer = [0; 1028];
+                let n = stream.read(&mut buffer).unwrap();
+                let request = Request::from_slice(&buffer[..n]).unwrap();
+
+                if request.path == "/" {
+                    let _ = stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n");
+                } else {
+                    let _ = stream.write_all(b"HTTP/1.1 404 NOT FOUND\r\n\r\n");
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+enum Method {
+    Get,
+    Post,
+    Put,
+    Delete,
+    Options,
+}
+
+enum Version {
+    Http1_1,
+}
+
+#[allow(dead_code)]
+struct Request {
+    method: Method,
+    pub path: String,
+    version: Version,
+}
+
+impl Request {
+    fn from_slice(bytes: &[u8]) -> std::io::Result<Self> {
+        let request_string = String::from_utf8_lossy(bytes);
+        let lines: Vec<&str> = request_string.split("\r\n").collect();
+        let first_line: Vec<&str> = lines[0].split(' ').collect();
+        let method = match first_line[0] {
+            "GET" => Method::Get,
+            "POST" => Method::Post,
+            "PUT" => Method::Put,
+            "DELETE" => Method::Delete,
+            "OPTIONS" => Method::Options,
+            unknown => {
+                unreachable!("got unknown method: {unknown}");
+            }
+        };
+        let path = first_line[1].to_owned();
+        let version = match first_line[2] {
+            "HTTP/1.1" => Version::Http1_1,
+            unknown => {
+                unreachable!("got unknown version: {unknown}");
+            }
+        };
+
+        Ok(Self {
+            method,
+            path,
+            version,
+        })
     }
 }
